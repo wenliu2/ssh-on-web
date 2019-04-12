dca16135bbfc3de6a8256c1f07d1f68b9eed445d
yuntang <yuntang@ebay.com>
2019-04-12 08:55:02 +0800

    commit for fix pipeline

--- JenkinsStage/Stage-4.sh
+++ JenkinsStage/Stage-4.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+
 ./envsubst < ./k8s-deploy/mongo-deploy.yaml | ./kubectl --kubeconfig $KUBECONFIG apply -f -
 ./envsubst < ./k8s-deploy/ssh-on-web-deploy.yaml | ./kubectl --kubeconfig $KUBECONFIG apply -f -
