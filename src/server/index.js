ab66e613fe8c4061ab285cbf34ad5f04b9132ed0
victor <tonyyunhan1997@163.com>
2019-10-07 08:49:34 +0800

    fixed: ssl for websocket

--- src/server/index.js
+++ src/server/index.js
@@ -11,15 +11,35 @@ const passport = require('passport')
 
 import api from './api/api'
 
+const https = require('https')
+const http = require('http')
+const fs = require('fs')
+
 const app = express();
-const expressWs = require('express-ws')(app)
+const ssl = config.get('ssl');
+const {
+  key_path,
+  cert_path,
+  ca_path
+} = ssl;
+
+let httpServer;
+if (ssl.on && fs.existsSync(key_path) && fs.existsSync(cert_path) && fs.existsSync(ca_path)) {
+  const options = {
+    key: fs.readFileSync(key_path),
+    cert: fs.readFileSync(cert_path),
+    ca: fs.readFileSync(ca_path)
+  };
+  httpServer = https.createServer(options, app);
+} else {
+  httpServer = http.createServer(app);
+}
+const expressWs = require('express-ws')(app, httpServer)
+
 const ws = require('./websocket')
 
 const auth = require('./auth')
 const bodyParser = require('body-parser')
-const https = require('https')
-const http = require('http')
-const fs = require('fs')
 
 import db from './db/'
 
@@ -78,27 +98,12 @@ app.use('/ws', passport.authenticate('jwt', {
 
 // start server
 const port = config.get('port');
-const ssl = config.get('ssl');
-const {
-  key_path,
-  cert_path,
-  ca_path
-} = ssl;
-if (ssl.on && fs.existsSync(key_path) && fs.existsSync(cert_path) && fs.existsSync(ca_path)) {
-  const options = {
-    key: fs.readFileSync(key_path),
-    cert: fs.readFileSync(cert_path),
-    ca: fs.readFileSync(ca_path)
-  };
-  https.createServer(options, app).listen(port, () => {
-    logger.debug(`Listening on port ${port}!`)
+
+httpServer.listen(port, () => {
+  logger.debug(`Listening on port ${port}!`)
+  if (ssl.on) {
     logger.debug(`SSL key file ${options.key}!`)
     logger.debug(`SSL cert file ${options.cert}!`)
-    db.connect()
-  });
-} else {
-  http.createServer(app).listen(port, () => {
-    logger.debug(`Listening on port ${port}!`)
-    db.connect()
-  });
-}
+  }
+  db.connect()
+});
